191dea78006a721c7c5ace164e7a4ff0ca2c1cb9
arian-jo <arian.asdfghqwerty@gmail.com>
2025-03-04 13:40:22 -0300

    Agregada autenticación con Google y integración de PayPal

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nombre-del-repositorio/', // Reemplaza con el nombre de tu repositorio
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
